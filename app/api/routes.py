bc9a1f8ed206ffdc86fdcf1845268e727a61cdf0
MrGecko <vrlitwhai.kridanik@gmail.com>
2018-09-14 11:17:56 +0200

    refactor the model facades & routes generation

--- app/api/routes.py
+++ app/api/routes.py
@@ -6,14 +6,26 @@ from app.api.response_factory import JSONAPIResponseFactory  as RF
 from tests.data.fixtures.entry import load_fixtures
 
 
-
 @api_bp.route('/api/<api_version>/init')
 def api_init(api_version):
     load_fixtures(db)
     return Response("init ok")
 
 
-def make_get_route(obj_getter, facade, rel_name, rel_links_fetcher, rel_resource_getter, rel_resource_identifier_fetcher):
+def register_get_route(obj_getter, facade, rel_name):
+    """
+
+    :param obj_getter:
+    :param facade:
+    :param rel_name:
+    :param rel_links_fetcher:
+    :param rel_resource_getter:
+    :param rel_resource_identifier_fetcher:
+    :return:
+    """
+    # ===============================
+    # relationships self link route
+    # ===============================
     rule = '/api/<api_version>/{type_plural}/<id>/relationships/{rel_name}'.format(
         type_plural=facade.TYPE_PLURAL, rel_name=rel_name
     )
@@ -23,13 +35,17 @@ def make_get_route(obj_getter, facade, rel_name, rel_links_fetcher, rel_resource
         if entry is None:
             return RF.make_errors_response(errors, **kwargs)
         else:
-            f = facade(entry)
-            data = {**(rel_links_fetcher(f)), "data": rel_resource_identifier_fetcher(f)}
+            relationship = facade(entry).relationships[rel_name]
+            data = {"links": relationship["links"], "data": relationship["resource_identifier"]}
             return RF.make_response(data, **kwargs)
 
-    resource_identifier_endpoint.__name__ = "%s_%s_%s" % (facade.TYPE_PLURAL, rel_name, resource_identifier_endpoint.__name__)
+    resource_identifier_endpoint.__name__ = "%s_%s_%s" % (facade.TYPE_PLURAL, rel_name.replace("-", "_"), resource_identifier_endpoint.__name__)
+    # register the rule
     api_bp.add_url_rule(rule, endpoint=resource_identifier_endpoint.__name__, view_func=resource_identifier_endpoint)
 
+    # ===================================
+    # relationships related link route
+    # ===================================
     rule = '/api/<api_version>/{type_plural}/<id>/{rel_name}'.format(
         type_plural=facade.TYPE_PLURAL, rel_name=rel_name
     )
@@ -39,12 +55,18 @@ def make_get_route(obj_getter, facade, rel_name, rel_links_fetcher, rel_resource
         if entry is None:
             return RF.make_errors_response(errors, **kwargs)
         else:
-            f = facade(entry)
-            return RF.make_data_response(rel_resource_getter(f))
+            relationship = facade(entry).relationships[rel_name]
+            return RF.make_data_response(relationship["resource"])
 
-    resource_endpoint.__name__ = "%s_%s_%s" % (facade.TYPE_PLURAL, rel_name, resource_endpoint.__name__)
+    resource_endpoint.__name__ = "%s_%s_%s" % (facade.TYPE_PLURAL, rel_name.replace("-", "_"), resource_endpoint.__name__)
+    # register the rule
     api_bp.add_url_rule(rule, endpoint=resource_endpoint.__name__, view_func=resource_endpoint)
 
 
+# =====================================
+# import api routes
+# =====================================
 from app.api.entry import routes
 from app.api.insee_commune import routes
+from app.api.alt_orth import routes
+
