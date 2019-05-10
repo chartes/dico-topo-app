5bd043c218cddcbfdfd1f395738ffbeacec7453e
MrGecko <vrlitwhai.kridanik@gmail.com>
2019-05-10 12:45:56 +0200

    remove the old frontend and keep only the API project in this repo

--- app/dicotopo_client/src/js/lib/utils.js
+++
@@ -1,9 +0,0 @@
-
-export function getUrlParameter(url, paramName) {
-    paramName = paramName.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    var regex = new RegExp('[\\?&]' + paramName + '=([^&#]*)');
-    var results = regex.exec(url);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-}
-
-export default getUrlParameter;
