b764ca69dbf642adf79e1c2bb289c8d2249b16d3
MrGecko <vrlitwhai.kridanik@gmail.com>
2019-05-15 10:55:48 +0200

    remove frontend reference from the README

--- README.md
+++ README.md
@@ -8,11 +8,4 @@ pip install -r requirements.txt
 ```
 gunicorn flask_app:flask_app -c gunicorn.conf.py
 ```
-3) Then visit http://127.0.0.1:5003/dico-topo
-
-Eventually builds the frontend to the latest version:
-```
-cd app/dicotopo_client
-npm install
-npm run build
-```
+3) Then visit http://127.0.0.1:5003/dico-topo/api/1.0?capabilities to get infos about the API capabilities
--- app/routes.py
+++ app/routes.py
@@ -3,38 +3,3 @@ from flask_jwt_extended import unset_jwt_cookies, set_access_cookies, create_acc
 from flask_login import current_user
 
 from app import app_bp, api_bp
-
-
-#@app_bp.route("/")
-#def index():
-#    resp = make_response(render_template("main/index.html"))
-#
-#    user = current_user
-#    if not user.is_anonymous:
-#        access_token = create_access_token(identity=user.to_json())
-#        resp.headers["login"] = True
-#        set_access_cookies(resp, access_token)
-#    else:
-#        resp.headers["logout"] = True
-#        unset_jwt_cookies(resp)
-#
-#    return resp, 200
-#
-#
-#@app_bp.route("/documentation")
-#def documentation():
-#    return render_template("docs/docs.html")
-#
-#
-#@app_bp.route('/placenames')
-#def get_placename_collection():
-#    num_start_page = request.args.get("filter[num_start_page]", 1)
-#    return render_template("main/placenames.html", num_start_page=num_start_page)
-#
-#
-#@app_bp.route('/placenames/<placename_id>')
-#def get_placename(placename_id):
-#    placename_url = url_for('api_bp.placenames_single_obj_endpoint', id=placename_id)
-#    return render_template("main/placename.html", placename_url=placename_url)
-
-
