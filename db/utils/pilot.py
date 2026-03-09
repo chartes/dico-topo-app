a266e845f89d8f489fddbc1c29b1c7f52acea411
architexte <architexte.git@gmail.com>
2026-03-09 22:43:29 +0100

    add DT03 and DT53 bibl

--- db/utils/pilot.py
+++ db/utils/pilot.py
@@ -20,8 +20,8 @@ cursor.execute('PRAGMA foreign_keys=ON')
 
 
 # 1. Avant insertion : calcul de nouveaux ids à injecter dans la source XML
-dt2db.new_ids_generator_tsv(db, cursor, 'DT29')
-exit()
+# dt2db.new_ids_generator_tsv(db, cursor, 'DT53')
+# exit()
 
 # création du user
 u1 = {"id": 1, "username": "delisle", "is_admin": 1}
@@ -48,16 +48,17 @@ insee.insert_longlat(db, cursor, 'tsv')
 # insee.update_insee_ref(db, cursor)
 
 DT_with_insee = [
-    "DT01", "DT02", "DT05", "DT07", "DT10",
-    "DT11", "DT14", "DT15", "DT18", "DT21",
-    "DT22", "DT23", "DT24", "DT26", "DT27",
-    "DT28", "DT29", "DT30", "DT34", "DT36",
-    "DT41", "DT42", "DT43", "DT44", "DT51",
-    "DT52", "DT54", "DT55", "DT56", "DT57",
-    "DT58", "DT60", "DT62", "DT64", "DT68",
-    "DT71", "DT72", "DT76", "DT77", "DT79",
-    "DT80", "DT86", "DT88", "DT89"]
-DT_with_insee = ["DT29"]
+    "DT01", "DT02", "DT03", "DT05", "DT07",
+    "DT10", "DT11", "DT14", "DT15", "DT18",
+    "DT21", "DT22", "DT23", "DT24", "DT26",
+    "DT27", "DT28", "DT29", "DT30", "DT34",
+    "DT36", "DT41", "DT42", "DT43", "DT44",
+    "DT51", "DT52", "DT53", "DT54", "DT55",
+    "DT56", "DT57", "DT58", "DT60", "DT62",
+    "DT64", "DT68", "DT71", "DT72", "DT76",
+    "DT77", "DT79", "DT80", "DT86", "DT88",
+    "DT89"]
+DT_with_insee = ["DT53"]
 
 for dt_id in DT_with_insee:
     dpt_code = dt_id[-2:]
